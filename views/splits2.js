21fcae280f69473c415cbf7153be60055c38eee0
leahsteinberg <leahsteinberg1@gmail.com>
2014-06-02 17:27:07 -0700

    implemented ability to pay multiple people

--- views/splits2.js
+++ views/splits2.js
@@ -14,16 +14,12 @@
 	var SplitsiesModule = React.createClass({
 			getInitialState: function(){
 				return (
-				{'items':[], 'item_name': '', 'item_price': '', 'payment_to': [], 'payment_price': '', 'payments': [], 'friends': [], 'roommates': [], 'user': {}, 'search_name': ''
-					});
+				{'items':[], 'item_name': '', 'item_price': '', 'payment_forms': [{'payment_to': '', 'payment_price': '', 'number': 0}], 'payments': [], 'friends': [], 'roommates': [], 'user': {}, 'search_name': '', 'pay_same': false});
 			},
 			loadItemsFromServer: function(){
 			},
 			getMoreFriends: function(nexturl){
-				console.log("next url is,",  nexturl);
-
 				nexturl = nexturl.slice(nexturl.indexOf('/v1'));
-				console.log("next url2 is,",  nexturl);
 				var that = this;
 				var formData = {
 					'nexturl': nexturl,
@@ -37,7 +33,6 @@
 				}).done(function(data) {
 					that.state.friends.push.apply(that.state.friends, data['friends']);
 					that.setState({'friends': that.state.friends});
-					console.log(that.state);
 					if(data['nexturl']){
 						that.getMoreFriends(data['nexturl']);
 					}	
@@ -59,7 +54,6 @@
 					url: '/venmo_user_friends',
 					dataType: 'json'
 				}).done(function(data) {
-					console.log("data is", data);
 					that.setState({'friends': data['friends']});
 					if(data['nexturl']){
 						that.getMoreFriends(data['nexturl']);
@@ -69,34 +63,26 @@
 				});
 			},
 			handlePaymentSubmit: function(e){
-				if(this.state.payment_to.length > 0 && !isNaN(this.state.payment_price)){
-					this.state.payments.push({'payment_to': this.state.payment_to[0], 'price': this.state.payment_price, 'payment_from': this.state.user});
-					this.setState({'payment_to': []});
-					this.setState({'payment_price': ''});
-				}
 				e.preventDefault();
-				
+				for(var i= 0; i< this.state.payment_forms.length; i++){
+					this.state.payments.push({'payment_to': this.state.payment_forms[i]['payment_to'], 'payment_price': this.state.payment_forms[i]['payment_price'], 'payment_from': this.state.user});
+				}
+				this.setState({'payment_forms': [{'payment_to': '', 'payment_price': '', 'number': 0}]})
 			},
 			handleItemSubmit: function(e){
 				e.preventDefault();
 				var who_owns = [];
-				console.log(this);
-				console.log("rooomates", this.state.roommates);
+
 				for(var i =0; i<this.state.roommates.length; i++){
-					console.log("handle item submit in for loop");
-					console.log(this.state.roommates[i]);
 					who_owns.push({'person': this.state.roommates[i], 'percent_owned': (1/(this.state.roommates.length+1))*100, 'percent_paid': 0});
 				}
-				who_owns.push({'person': this.state.user, 'percent_owned': (1/(this.state.roommates.length+1))*100, percent_paid: 100})
-				console.log(who_owns);
-				
+				who_owns.push({'person': this.state.user, 'percent_owned': (1/(this.state.roommates.length+1))*100, percent_paid: 100})				
 				this.state.items.push({'item_name': this.state.item_name, 'price': this.state.item_price, 'people':who_owns});
 				this.setState({'item_name': ''});
 				this.setState({'item_price': ''});
-				//e.preventDefault();
-
 			},
-			handleChange: function(name, e){
+			handleChange: function(name, number, e){
+				e.preventDefault();
 				if(name === 'item_name'){
 					this.setState({'item_name': e.target.value});
 				}
@@ -104,10 +90,18 @@
 					this.setState({'item_price': e.target.value});
 				}
 				else if(name === 'payment_price'){
-					this.setState({'payment_price': e.target.value});
+					for(var i =0; i< this.state.payment_forms.length; i++){
+						if(this.state.payment_forms[i]['number'] === number){
+							this.state.payment_forms[i]['payment_price'] = e.target.value;
+						}
+					}
+					this.setState({'payment_forms': this.state.payment_forms});
 				}
+				console.log("IN HANDLE CHANGE , item name, ", this.state.item_name, "item_price", this.state.item_price);
 			},
 			selectAsRoommate: function(name, e){
+				console.log("in select AS roommate: name is: ", name, "e is: ", e);
+
 				var indexInRoommates = objInObjList(name.props, this.state.roommates, 'id');
 				if(indexInRoommates !== -1){
 					this.state.roommates.splice(indexInRoommates, 1);
@@ -120,23 +114,32 @@
 
 			},
 			selectRoommate: function(name, e){
-				if(this.state['payment_to'].length> 0){
-					this.state.payment_to.splice(0,1);
+				for(var i = 0; i< this.state.payment_forms.length; i++){
+					if(this.state.payment_forms[i]['number'] === name.props.number){
+						this.state.payment_forms[i]['payment_to'] = {'display_name': name.props.display_name, 'id': name.props.id};
+					}
 				}
-				this.state.payment_to.push(name.props);
-				this.setState({'payment_to': this.state.payment_to});		
+				this.setState({'payment_forms': this.state.payment_forms});		
 			},
 			onSearchInput: function(e){
 				this.setState({'search_name': e.target.value});
 
 			},
+			addPaymentPerson: function(e){
+				e.preventDefault();
+				console.log("number peopel is: ", this.state);
+				new_number = this.state.payment_forms.length;
+				this.state.payment_forms.push({'payment_to': '', 'payment_price': '', 'number': new_number});
+				this.setState({'payment_forms': this.state.payment_forms});
+			},
 			render: function(){
 				return (<div>
-						<UserModule friends={this.state.friends} user={this.state.user_name} roommates={this.state.roommates}  selectAsRoommate={this.selectAsRoommate}/>
-						<newPayment roommates={this.state.roommates} payment_to={this.state.payment_to} selectRoommate={this.selectRoommate} payment_price={this.state.payment_price} onChange={this.handleChange} handlePaymentSubmit={this.handlePaymentSubmit}/>
-						<newItem handleItemSubmit={this.handleItemSubmit} item_name={this.state.item_name} item_price={this.state.item_price} onChange={this.handleChange} />
+						<div id="sideBar">
 						<PeopleInfo items={this.state.items}  payments={this.state.payments} roommates={this.state.roommates}/>
-						<ItemsInfo items={this.state.items}/>
+						<ItemsInfo items={this.state.items}/> </div>
+						<UserModule friends={this.state.friends} user={this.state.user_name} roommates={this.state.roommates}  selectAsRoommate={this.selectAsRoommate} />
+						<newPayment addPaymentPerson={this.addPaymentPerson} roommates={this.state.roommates} payment_forms={this.state.payment_forms} selectRoommate={this.selectRoommate} onChange={this.handleChange} handlePaymentSubmit={this.handlePaymentSubmit} />
+						<newItem handleItemSubmit={this.handleItemSubmit} item_name={this.state.item_name} item_price={this.state.item_price} onChange={this.handleChange} />
 						</div>);
 			}
 		});
@@ -146,7 +149,7 @@
 				var items = this.props.items.map(function(item){
 					return (<Item item_name={item['item_name']}  price={item['price']}/>);
 				});
-				return (<div><p>Heres the items:</p>{items}</div>);
+				return (<div id="itemsInfo"><p class="bold">Heres the items:</p>{items}</div>);
 			}
 		});
 
@@ -159,12 +162,12 @@
 
 		var newItem = React.createClass({
 			render: function(){
-				return (<div>
+				return (<div id="newItem" >
 					<form onSubmit={this.props.handleItemSubmit}> I bought
 						<input type ="text"  value={this.props.item_name} 
-						onChange={this.props.onChange.bind(this, 'item_name')}>
+						onChange={this.props.onChange.bind(this, 'item_name', 0)}>
 						for this much: </input>
-						<input type ="text" value={this.props.item_price} onChange={this.props.onChange.bind(this, 'item_price')}></input>
+						<input type ="text" value={this.props.item_price} onChange={this.props.onChange.bind(this,'item_price', 0)}></input>
 						<button>new item</button>
 					</form>
 				</div>);
@@ -181,26 +184,23 @@
 					var balance = getBalance(person, items, payments);
 					return (<PersonInfo display_name={person['display_name']} balance={balance}/>);
 				});
-				return (<div><p>Heres the people:</p>{peopleComponents}</div>);
+				return (<div id="peopleInfo"><p class="bold">Roommates:</p>{peopleComponents}</div>);
 			}
 		});
 
 
 		var PersonInfo = React.createClass({
 			render: function(){
-				return (<div>{this.props.display_name}, ${this.props.balance}</div>);
+				return (<div id="personInfo" >{this.props.display_name}, ${this.props.balance}</div>);
 			}
 		});
 
-
-
 		var getBalance = function(person, items, payments){
+			console.log("in GET BALANCE: items ", items);
 			var balance=0;
-			console.log("items in get balance,", items);
 			for(var i=0; i< items.length; i++){
 				for(var j =0; j< items[i]['people'].length; j++){
 					if(person['id'] === items[i]['people'][j]['person']['id']){
-						console.log("percent owned ", items[i]['people'][j].percent_owned);
 						balance-=(items[i].people[j].percent_owned/100)*items[i].price;
 						balance+=(items[i].people[j].percent_paid/100)*items[i].price;
 					}
@@ -209,10 +209,11 @@
 			var payment_balance =0;
 			for(var i =0; i<payments.length; i++){
 				if(person['display_name'] === payments[i].payment_to['display_name']){
-					balance+=parseInt(payments[i]['price']);
+					console.log(payments[i]);
+					balance+=parseInt(payments[i]['payment_price']);
 				}
 				else if(person === payments[i]['payment_from']){
-					balance-=payments[i]['price'];
+					balance-=payments[i]['payment_price'];
 
 				}
 			}
@@ -220,36 +221,42 @@
 		};
 
 
-
-
-
-
-
-
-
 		var newPayment = React.createClass({
+			render: function(){
+				var that = this;
+				people_to_pay = this.props.payment_forms.map(function(payment){
+					return (<payPerson number={payment.number} roommates={that.props.roommates} payment_to={payment.payment_to} selectRoommate={that.props.selectRoommate} payment_price={payment.payment_price} onChange={that.props.onChange} select_multiple={that.props.select_multiple}/>);
+				});
+				//for(var i = 0; i< this.state.number_people; i++){
+				//	people_to_pay.push(<payPerson key={i} roommates={this.props.roommates} payment_to={this.props.payment_to} selectRoommate={this.props.selectRoommate} payment_price={this.props.payment_price} onChange={this.props.onChange}/>);
+				//}
+				return (<div>
+					{people_to_pay}
+					<form onSubmit={this.props.handlePaymentSubmit}>
+					<button>new payment</button>
+					<button type="button" onClick={this.props.addPaymentPerson}>pay another person</button>
+					</form>
+				</div>);
+			}
+		});
+		var payPerson = React.createClass({
 			getInitialState: function(){
 				return {filter_text: ''};
-
 			},
 			onSearchInput: function(e){
 				this.setState({'filter_text': e.target.value});
 				e.preventDefault();
 			},
 			render: function(){
-				return (<div>
-						<p>Type to pick a roommate to pay:</p>
-						<SearchBar filter_text={this.state.filter_text} onChange={this.onSearchInput} />
-						<SelectPeople whole_list={this.props.roommates} select_list={this.props.payment_to} onSelect={this.selectRoommate} filter_text={this.state.filter_text} onSelect={this.props.selectRoommate}/>
-						 I paid
-						 <form onSubmit={this.props.handlePaymentSubmit}>
-					<input type ="text" value={this.props.payment_price} onChange={this.props.onChange.bind(this, 'payment_price')}></input>
-					<button>new payment</button>
-					</form>
-				</div>);
+				return(<div> 
+					<p>pick a roommate to pay:</p>
+				<SearchBar filter_text={this.state.filter_text} onChange={this.onSearchInput}/>
+				<SelectPeople whole_list={this.props.roommates} select_list={this.props.payment_to} filter_text={this.state.filter_text} onSelect={this.props.selectRoommate} number={this.props.number}/>
+				I paid
+				<input type ="text" value={this.props.payment_price} onChange={this.props.onChange.bind(this, 'payment_price', this.props.number)}></input>
+					</div>)
 			}
 		});
-
 
 		var UserModule = React.createClass({
 			componentWillMount: function(){
@@ -260,10 +267,9 @@
 			},
 			render: function(){
 				return(<div>{this.props.user_name}
-					<p>Type to search friends, to select roommates:</p>
+					<p>select roommates:</p>
 				<SearchBar filter_text={this.state.filter_text} onChange={this.onSearchInput}/>
-				<SelectPeople whole_list={this.props.friends} select_list={this.props.roommates} onSelect={this.props.selectAsRoommate} filter_text={this.state.filter_text}  />
-
+				<SelectPeople  whole_list={this.props.friends} select_list={this.props.roommates} onSelect={this.props.selectAsRoommate} filter_text={this.state.filter_text}  number={null}/>
 				</div>);
 			}
 		});
@@ -283,13 +289,13 @@
 				}
 				people = filtered_people.map(function(person){
 					if(objInObjList(person, that.props.select_list, 'id') !== -1){
-						return (<PersonToSelect id={person['id']} display_name={person['display_name']} onSelect={that.props.onSelect} is_selected={true}/>);
+						return (<PersonToSelect number={that.props.number_people} id={person['id']} display_name={person['display_name']} onSelect={that.props.onSelect} is_selected={true}/>);
 					}
 					else{
-						return (<PersonToSelect id={person['id']} display_name={person['display_name']} onSelect={that.props.onSelect}  is_selected={false}/>);
+						return (<PersonToSelect  number={that.props.number}  id={person['id']} display_name={person['display_name']} onSelect={that.props.onSelect}  is_selected={false}/>);
 					}
 				});
-				return (<div>{people}</div>);	
+				return (<div id="selectPeopleDiv"><select multiple size="10" id="selectPeopleSelect">{people}</select></div>);	
 			}
 		});
 
@@ -309,15 +315,16 @@
 			render: function(){
 				if(this.props.is_selected){
 					var divStyle = {
-					backgroundColor: '#86E2D5'
+					backgroundColor: '#13A3A5'
 					}
 				}
 				else{
 				var divStyle = {
-					backgroundColor: '#C5EFF7'
+					backgroundColor: '#FFFFFF'
+
 				};
 				}
-				return (<div style={divStyle} onClick={this.props.onSelect.bind(this.props, this)} value={this.props.display_name}>{this.props.display_name}</div>);
+				return (<option style={divStyle} onClick={this.props.onSelect.bind(this.props, this)} value={this.props.display_name}>{this.props.display_name}</option>);
 			}
 		});
 
